ccedd5ac91673e285e65d3d24137bdd20997cba8
Carlo Giacomoni <giacomoni.carlo@gmail.com>
2022-10-19 01:15:34 -0300

    initial commit

---
+++ src/test/java/com/gmdgold/pizzariafuturodev/PizzariaFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdgold.pizzariafuturodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
